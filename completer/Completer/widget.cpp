9f2d659b54e7905f3c42ade46236713e1e9eb084
songwentao <614425032@qq.com>
2019-12-09 21:52:55 +0800

    1、Qt模糊搜索功能

---
+++ completer/Completer/widget.cpp
@@ -0,0 +1,22 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+#include <QCompleter>
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    QStringList wordList;
+     wordList << QStringLiteral("信号源1") << QStringLiteral("信号源2") <<  QStringLiteral("无纸化") << QStringLiteral("左中右三屏台");
+
+     QCompleter *completer = new QCompleter(wordList, this);
+     completer->setCaseSensitivity(Qt::CaseInsensitive);
+     completer->setFilterMode(Qt::MatchContains);
+     ui->lineEdit->setCompleter(completer);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
